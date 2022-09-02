fe96a624af61f07b18f2a18ee5c7b43020605c77
shuntian <978987373@qq.com>
2022-09-02 22:44:58 +0800

    chore: add middleware for json-server

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import ProjectList from 'screens/project-list';
+import { LoginScreen } from 'screens/login';
+// import ProjectList from 'screens/project-list';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <ProjectList />
+      {/* <ProjectList /> */}
+      <LoginScreen />
     </div>
   );
 }
