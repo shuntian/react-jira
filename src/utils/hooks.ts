ed57638d1d9c166911994e9616f2838f2bed5f28
shuntian <978987373@qq.com>
2022-09-05 21:28:39 +0800

    feat: add use async hook

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -28,9 +28,70 @@ export const useArray = <T>(param: T[]) => {
     setValue,
     add: (item: T) => setValue([...value, item]),
     clear: () => setValue([]),
-    remvoeIndex: (index: number) => {
+    removeIndex: (index: number) => {
       const newValue = [...value];
       setValue(newValue.splice(index, 1));
     },
+  };
+};
+
+interface State<D> {
+  error: Error | null;
+  data: D | null;
+  stat: 'idle' | 'loading' | 'error' | 'success';
+}
+
+const defaultInitialState: State<null> = {
+  error: null,
+  data: null,
+  stat: 'idle',
+};
+
+export const useAsync = <D>(initialState?: State<D>) => {
+  const [state, setState] = useState<State<D>>({
+    ...defaultInitialState,
+    ...initialState,
+  });
+
+  const setData = (data: D) =>
+    setState({
+      error: null,
+      stat: 'success',
+      data: data,
+    });
+
+  const setError = (error: Error | null) =>
+    setState({
+      error: error,
+      stat: 'error',
+      data: null,
+    });
+
+  const run = (promise: Promise<D>) => {
+    if (!promise || !promise.then) {
+      throw new Error('请传入一个 promise 对象');
+    }
+
+    setState({ ...state, stat: 'loading' });
+    return promise
+      .then((data) => {
+        setData(data);
+        return data;
+      })
+      .catch((error) => {
+        setError(error);
+        return error;
+      });
+  };
+
+  return {
+    isIdle: state.stat === 'idle',
+    isLoading: state.stat === 'loading',
+    isError: state.stat === 'error',
+    isSuccess: state.stat === 'success',
+    run,
+    setData,
+    setError,
+    ...state,
   };
 };
